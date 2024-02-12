5ca001b355ad22e5e963c45ef83192844042bf22
staranjot-singh <159674655+staranjot-singh@users.noreply.github.com>
2024-02-12 23:45:37 +0000

    updated version

--- lab3.py
+++ lab3.py
@@ -2,3 +2,8 @@ def write_text_file():
     with open("your_name.txt", "w") as file:
         file.write("Your Name")
     print("Text file created successfully.")
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
